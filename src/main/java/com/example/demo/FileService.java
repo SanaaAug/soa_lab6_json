0693a869fd06d89e34ea74b5d91184f4d4ed8dad
sanaa <sanaa040819@gmail.com>
2026-03-26 20:22:31 +0800

    G

--- src/main/java/com/example/demo/FileService.java
+++ src/main/java/com/example/demo/FileService.java
@@ -1,18 +1,17 @@
 package com.example.demo;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
+import java.net.URI;
 
 @Service
 public class FileService {
@@ -29,29 +28,32 @@ public class FileService {
     @Value("${s3.bucket}")
     private String bucketName;
 
-    private AmazonS3 s3;
+    private S3Client s3;
 
     @PostConstruct
     public void init() {
-        BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
-        this.s3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(
-                new AwsClientBuilder.EndpointConfiguration(endpoint, "sgp1"))
-            .withCredentials(new AWSStaticCredentialsProvider(creds))
-            .withPathStyleAccessEnabled(true)
+        AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey, secretKey);
+        this.s3 = S3Client.builder()
+            .endpointOverride(URI.create(endpoint))
+            .credentialsProvider(StaticCredentialsProvider.create(creds))
+            .region(Region.of("sgp1"))
+            .forcePathStyle(true)
             .build();
     }
 
     public String uploadFile(MultipartFile file) throws IOException {
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+        PutObjectRequest request = PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(fileName)
+            .contentType(file.getContentType())
+            .contentLength(file.getSize())
+            .acl("public-read")
+            .build();
 
-        s3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata)
-            .withCannedAcl(CannedAccessControlList.PublicRead));
+        s3.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
-        return s3.getUrl(bucketName, fileName).toString();
+        return endpoint + "/" + bucketName + "/" + fileName;
     }
 }
